49633d1ba4d75f8dbcc4d4c69d05ee583f1f54a9
developercodingarrow <digitalcloudinfo@gmail.com>
2024-02-12 18:03:46 +0530

    first commit

---
+++ Actions/cityAction.js
@@ -0,0 +1,28 @@
+import { performAPIAction, performGetAPIAction } from "./performAPIAction";
+import { getLoginCookies } from "./authAction";
+import { API } from "../config";
+
+const loginToken = getLoginCookies();
+
+console.log(API);
+
+// API for CREATE NEW USER
+export const createNewCity = async (requestData) => {
+  const url = `${API}/city/create-city`;
+  const method = "post";
+  return performAPIAction(method, url, requestData, loginToken);
+};
+
+export const deleteCity = async (requestData) => {
+  const data = {
+    id: requestData,
+  };
+  const url = `${API}/city/delete-city`;
+  const method = "DELETE";
+  return performAPIAction(method, url, data, loginToken);
+};
+
+export const allCity = async () => {
+  const url = `${API}/city/all-cites`;
+  return performGetAPIAction(url);
+};
---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  publicRuntimeConfig: {
+    App_NAME: "Saransh Inadia",
+    API_DEVELOPMENT: "http://localhost:8000/api/v1/saranshrealtorsindia",
+    API_PRODUCTION: "http://3.110.242.244/api/v1/saranshrealtorsindia",
+    DOMAIN_DEVELOPMENT: "http://localhost:3000/",
+    DOMAIN_PRODUCTION: "http://3.110.242.244/",
+    PEODUCTION: false,
+  },
+};
